03611cf52b6d84755d0bb0971ca6ff90411238aa
Martin Vladimirov <martinvlad@MacBook-Pro-3.local>
2022-09-30 16:45:35 -0700

    added changes and started list component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import {DateRange} from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; 
 import {format} from 'date-fns';
-export default function Header() {
+export default function Header({type }) {
     const [date, setDate] = useState([
         {
           startDate: new Date(),
@@ -21,9 +21,18 @@ export default function Header() {
         children: 0,
         roomNumber: 1
       });
+
+      function onButtonChange (name, operation){
+        
+        setOptions((prevState) =>{
+          return {
+            ...prevState, [name]: operation == 'increase' ? options[name] + 1 : options[name] - 1
+          }
+        })
+      }
   return (
     <div className='header'>
-        <div className="headerContainer">
+        <div className={type == 'list' ? 'headerContainer listMode' : 'headerContainer'}>
 
         <div className="headerList">
             <div className="headerListItem active">
@@ -47,8 +56,8 @@ export default function Header() {
             <span>Airport Taxis</span>
             </div>
         </div>
-        <h1 className="headerTitle">A lifetime of discounts? It's genius!</h1>
-        <p className="headerDesc">Get rewarded for your travels - unlock instant savings of 10% or more with a free Lamabooking account</p>
+        { type !== 'list' && <><h1 className="headerTitle">A lifetime of discounts? It's genius!</h1>
+        <p className="headerDesc">Get rewarded for your travels - unlock instant savings of 10% or more with a free MartinVladimirov account</p>
         <button className='headerBtn'>Sign In / Register</button>
         <div className="headerSearch">
             <div className="headerSearchItem">
@@ -68,12 +77,39 @@ export default function Header() {
             </div>
             <div className="headerSearchItem">
             <FontAwesomeIcon icon={faPerson} className='headerIcon' />
-            <span className='headerSearchText'>{`${options.adult} adult ${options.children} children ${options.roomNumber} room`}</span>
+            <span onClick={() => setOptionsShow(!optionsShow)} className='headerSearchText'>{`${options.adult} adult ${options.children} children ${options.roomNumber} room`}</span>
+           {optionsShow && <div className="options">
+            <div className="optionItem">
+              <span className="optionText">Adult</span>
+              <div className="optionCounter">
+              <button disabled={options.adult <= 1 ? true: false} className="optionCounterButton" onClick={() => onButtonChange('adult', 'decrease')}> -</button>
+              <span className="optionCounterText"> {options.adult}</span>
+              <button className="optionCounterButton" onClick={() => onButtonChange('adult', 'increase')}> + </button>
+              </div>
+            </div>
+            <div className="optionItem">
+              <span className="optionText">Children</span>
+              <div className="optionCounter">
+              <button disabled={options.children <= 0 ? true: false} className="optionCounterButton" onClick={() => onButtonChange('children', 'decrease')}> -</button>
+              <span className="optionCounterText">{options.children}</span>
+              <button className="optionCounterButton" onClick={() => onButtonChange('children', 'increase')}> + </button>
+              </div>
+            </div>
+            <div className="optionItem">
+              <span className="optionText">Room</span>
+              <div className="optionCounter">
+              <button disabled={options.roomNumber <= 1 ? true: false} className="optionCounterButton" onClick={() => onButtonChange('roomNumber', 'decrease')}> -</button>
+              <span className="optionCounterText">{options.roomNumber}</span>
+              <button className="optionCounterButton" onClick={() => onButtonChange('roomNumber', 'increase')}> + </button>
+              </div>
+            </div>
+           </div>}
+           
             </div>
             <div className="headerSearchItem">
                 <button className="headerBtn">Search</button>
             </div>
-        </div>
+        </div></>}
         </div>
     </div>
   )
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
 
 export default function List() {
-  return <div>List</div>;
+  return (
+    <div>
+      <NavBar />
+      <Header type="list" />
+    </div>
+  );
 }
